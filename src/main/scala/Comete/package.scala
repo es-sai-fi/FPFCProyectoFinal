14a6787cd71d1b985954ba37c829af2fc7685f1f
es-sai-fi <jhormangomez2005@outlook.com>
2024-11-02 21:41:14 -0500

    min_p, rhoCMT_Gen, normalizar added

---
+++ src/main/scala/Comete/package.scala
@@ -0,0 +1,66 @@
+package object Comete {
+  type DistributionValues = Vector[Double]
+  // Tipo para los valores, reales ordenados entre 0 y 1,
+  // incluidos 0 y 1, de una distribucion
+
+  type Frequency = Vector[Double]
+  // Pi k es una frecuencia de longitud k
+  // si Pi k.lenght=k, 0<=Pi k(i)<=1, 0<=i<=k-1
+  // y Pi k.sum==1
+
+  type Distribution = (Frequency, DistributionValues)
+  // (Pi, dv) es una distribucion si pi es una Frecuencia
+  // y dv son los valores de distribucion y pi y dv son
+  // de la misma longitud
+
+  type PolMeasure = Distribution => Double
+
+  def min_p(f:Double=>Double, min:Double, max:Double, prec:Double):Double = {
+    // Devuelve el punto p en [min,max] tal que f(p) es minimo
+    // Suponiendo que f es convexa
+    // si max-min<prec, devuelve el punto medio de [min, max]
+    if ((max-min)<prec) (min+max)/2.0 else {
+      val divideBy = 10.0
+      val intervalLength = (max-min)/divideBy
+
+      val points = (BigDecimal(min) to BigDecimal(max) by intervalLength).map(x => (x.doubleValue, f(x.doubleValue)))
+
+      val (minP, _) = points.minBy(_._2)
+
+      val newMin = Math.max(min, minP-intervalLength/2.0)
+      val newMax = Math.min(max, minP+intervalLength/2.0)
+
+      min_p(f, newMin, newMax, prec)
+    }
+  }
+
+  def rhoCMT_Gen(alpha:Double, beta:Double):PolMeasure = {
+    // Dados alpha y beta devuelve la funcion que calcula la medida
+    // comete parametrizada en alpha y beta
+    (distribution: Distribution) => {
+      val frequency = distribution._1
+      val distributionValues = distribution._2
+      val tuples = frequency.zip(distributionValues)
+
+      def sum(p: Double, tuples: Seq[(Double, Double)]): Double = {
+        tuples.map{ case (pi,y) => Math.pow(pi, alpha) * Math.pow(Math.abs(y-p), beta)
+        }.sum
+      }
+
+      val f = (p: Double) => sum(p, tuples)
+
+      val min = min_p(f, 0.0, 1.0, 0.1)
+
+      f(min)
+    }
+  }
+
+  def normalizar(m:PolMeasure):PolMeasure = {
+    // Recibe una medida de polarizacion, y devuelve la
+    // correspondiente medida que la calcula normalizada
+    (distribution: Distribution) => {
+      val pol = m(distribution)
+      pol/0.379
+    }
+  }
+}
---
+++ src/test/scala/Pruebas.sc
@@ -0,0 +1,38 @@
+import Comete._
+
+val pi_max = Vector(0.5 , 0.0, 0.0, 0.0, 0.5)
+val pi_min = Vector(0.0 , 0.0, 1.0, 0.0, 0.0)
+val pi_der = Vector(0.4 , 0.0, 0.0, 0.0, 0.6)
+val pi_izq = Vector(0.6 , 0.0, 0.0, 0.0, 0.4)
+val pi_int1 = Vector(0.0 , 0.5, 0.0, 0.5, 0.0)
+val pi_int2 = Vector(0.25, 0.0, 0.5, 0.0, 0.25)
+val pi_int3 = Vector(0.25, 0.25, 0.0, 0.25, 0.25)
+val pi_cons_centro = pi_min
+val pi_cons_der = Vector(0.0, 0.0, 0.0, 0.0, 1.0)
+val pi_cons_izq = Vector(1.0, 0.0, 0.0, 0.0, 0.0)
+val likert5 = Vector(0.0 , 0.25, 0.5, 0.75, 1.0)
+
+val cmt1 = rhoCMT_Gen(1.2, 1.2)
+
+cmt1(pi_max, likert5)
+cmt1(pi_min, likert5)
+cmt1(pi_der, likert5)
+cmt1(pi_izq, likert5)
+cmt1(pi_int1, likert5)
+cmt1(pi_int2, likert5)
+cmt1(pi_int3, likert5)
+cmt1(pi_cons_centro, likert5)
+cmt1(pi_cons_der, likert5)
+cmt1(pi_cons_izq, likert5)
+
+val cmt1_norm = normalizar(cmt1)
+cmt1_norm(pi_max, likert5)
+cmt1_norm(pi_min, likert5)
+cmt1_norm(pi_der, likert5)
+cmt1_norm(pi_izq, likert5)
+cmt1_norm(pi_int1, likert5)
+cmt1_norm(pi_int2, likert5)
+cmt1_norm(pi_int3, likert5)
+cmt1_norm(pi_cons_centro, likert5)
+cmt1_norm(pi_cons_der, likert5)
+cmt1_norm(pi_cons_izq, likert5)
